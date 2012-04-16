378b7ccbe85e65c6b7fe44eab300f02ea895f0b7
cody <sebastian.kotulla@gmail.com>
2012-04-16 21:08:45 +0200

    display help on console

--- src/cody/grblgui/desktop/GrblGuiMainDesktop.java
+++ src/cody/grblgui/desktop/GrblGuiMainDesktop.java
@@ -8,8 +8,15 @@ import cody.grblgui.Main;
 import com.badlogic.gdx.backends.jogl.JoglApplication;
 
 public class GrblGuiMainDesktop {
+	static void displayHelp() {
+		System.out.println("usage: java -jar grblgui.jar <gcode-file> <grbl-device-file>");
+		System.out.println("\texample: java -jar grblgui.jar bunny.g /dev/ttyACM0");
+	}
 	public static void main (String[] argv) {
-		
+		if(argv.length != 2) {
+			displayHelp();
+			return;
+		}
 		Main main = new Main(argv[0], argv[1]);
 
 		
